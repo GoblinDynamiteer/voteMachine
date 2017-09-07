fa7fc2d7df137a2aa3fe1f35946413c027e96b69
Johan Kämpe <kampe.johan@gmail.com>
2017-09-07 11:34:03 +0200

    Use global variables for setting text on display

--- voteMachine/voteMachine.ino
+++ voteMachine/voteMachine.ino
@@ -1,10 +1,13 @@
 #include <U8glib.h>
 
-/* Command buffer size / lenght */
+/* Command buffer size / length */
 #define SERIAL_BUFFER_SIZE 30
+#define LINE_HEIGHT 15
 
 const char COMMAND_END = '\n';
 U8GLIB_SSD1306_128X64 display(U8G_I2C_OPT_NONE);
+
+char * line[4] = { "one", "two", "three", "four"};
 
 void setup()
 {
@@ -19,9 +22,9 @@ void loop()
     data = readSerial();
 
     /* If command is not empty */
-    if(data[0] != '\0')
+    if(data[0])
     {
-        setText(data);
+        setText();
     }
 
     delay(50);
@@ -38,19 +41,27 @@ char * readSerial()
         SERIAL_BUFFER_SIZE
     );
 
+    // Skriv ut size till display -- rad 3
+
     command[size] = 0;
 
     return command;
 }
 
-void setText(char * text)
+/* Write text to display */
+void setText()
 {
     display.firstPage();
 
     do
     {
+        /* Använd 3 globala strängar för rader? */
         display.setFont(u8g_font_helvB10);
-        display.drawStr(0,15, text);
+
+        display.drawStr(0, 1 * LINE_HEIGHT, line[0]);
+        display.drawStr(0, 2 * LINE_HEIGHT, line[1]);
+        display.drawStr(0, 3 * LINE_HEIGHT, line[2]);
+        display.drawStr(0, 4 * LINE_HEIGHT, line[3]);
     }
     while(display.nextPage());
 
