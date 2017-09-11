88cfc413f148a12296a412c37fa8f044f3396757
Johan Kämpe <kampe.johan@gmail.com>
2017-09-11 15:42:32 +0200

    Port Arduino UNO -> NodeMCU

--- voteMachine/voteMachine.ino
+++ voteMachine/voteMachine.ino
@@ -1,43 +1,66 @@
-#include <U8glib.h>
+por/*
+    voteMachine!
+    by Johan K�mpe & Dylan Saleh
+
+    NodeMCU Pins:
+
+
+
+
+ */
+
+
+#include <ESP8266WiFi.h>
+#include <WiFiSettings.h> // SSID & Password
+#include <Wire.h>
+#include "SSD1306.h"
 
 #define SERIAL_BUFFER_SIZE 30
 #define LINE_HEIGHT 15
 #define BUTTON_DELAY 100
 const char COMMAND_END = '\n';
 
-const byte int_pin_green = 2;
-const byte int_pin_red = 3;
+const byte int_pin_green = D3;
+const byte int_pin_red = D1;
 
 unsigned long timer_green;
 unsigned long timer_red;
 
-U8GLIB_SSD1306_128X64 display(U8G_I2C_OPT_NONE);
+WiFiServer server(80);
+WiFiClient client;
+
+SSD1306 display(0x3c, D3, D5);
 
 char * line[4];
 int vote_green, vote_red;
 
 void setup()
 {
+    display.init();
+    display.flipScreenVertically();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+
     Serial.begin(9600);
     Serial.setTimeout(1000);
-
-    /* Display font */
-    display.setFont(u8g_font_helvB10);
+    delay(10);
 
     /* Interrupt pins */
     pinMode(int_pin_green, INPUT_PULLUP);
     pinMode(int_pin_red, INPUT_PULLUP);
 
     /* Setup interrupts */
-    attachInterrupt(digitalPinToInterrupt(int_pin_green), int_func_green, LOW);
-    attachInterrupt(digitalPinToInterrupt(int_pin_red), int_func_red, LOW);
+    attachInterrupt(digitalPinToInterrupt(
+        int_pin_green), int_func_green, FALLING);
+    attachInterrupt(digitalPinToInterrupt(
+        int_pin_red), int_func_red, FALLING);
 
     timer_red = millis();
     timer_green = millis();
 
     for (int i = 0; i < 4; i++)
     {
-        line[i] = malloc(10);
+        line[i] = (char *)malloc(10);
     }
 
     strcpy(line[0], "Set text");
@@ -51,7 +74,10 @@ void loop()
 {
     /* Get command from bluetooth serial */
     char * data;
-    data = readSerial();
+    do
+    {
+        data = readSerial();
+    } while(data[0] == '0');
 
     /* Switch on first char in sent command */
     switch (data[0])
@@ -136,17 +162,12 @@ char * readSerial()
 /* Write text to display */
 void setText()
 {
-    display.firstPage();
-
-    do
-    {
-        display.drawStr(0, 1 * LINE_HEIGHT, line[0]);
-        display.drawStr(0, 2 * LINE_HEIGHT, line[1]);
-        display.drawStr(0, 3 * LINE_HEIGHT, line[2]);
-        display.drawStr(0, 4 * LINE_HEIGHT, line[3]);
-    }
-    while(display.nextPage());
-
+    display.clear();
+    display.drawString(0, 0,  String(line[0]));
+    display.drawString(0, 10, String(line[1]));
+    display.drawString(0, 20, String(line[2]));
+    display.drawString(0, 30, String(line[3]));
+    display.display();
 }
 
 /* Green button interrupt function */
