e615a3d046937c547dbea411cffc5fa8368c1002
Johan Kämpe <kampe.johan@gmail.com>
2017-09-12 10:41:18 +0200

    Added sketch from port branch

--- voteMachine/voteMachine.ino
+++ voteMachine/voteMachine.ino
@@ -1,59 +1,100 @@
-#include <SSD1306.h>
+/*
+    voteMachine!
+    by Johan Kampe & Dylan Saleh
+
+    Hookup:
+
+    SSD1306     SDA D2
+    SSD1306     SCL D1
+    HC-06       RX  TX
+    HC-06       TX  RX
+    BUTTON_G    D6
+    BUTTON_R    D5
+
+
+ */
+
+#include <ESP8266WiFi.h>
+#include <WiFiSettings.h> // SSID & Password
+#include <Wire.h>
+#include "SSD1306.h"
+
+#define PIN_WIRE_SDA 4
+#define PIN_WIRE_SCL 5
+#define SDA PIN_WIRE_SDA
+#define SCL PIN_WIRE_SCL
+
+#define LED_BUILTIN 16
+#define BUILTIN_LED 16
+
+#define D0 1
+#define D1 5
+#define D2 4
+#define D3 0
+#define D4 2
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 15
+#define D9 3
+#define D10 1
 
 #define SERIAL_BUFFER_SIZE 30
-#define LINE_HEIGHT 15
-#define BUTTON_DELAY 100
+#define LINE_HEIGHT 10
+#define LINE_LENGHT 30
+#define MAX_LINES 5
+#define BUTTON_DELAY 200
 const char COMMAND_END = '\n';
 
-const byte int_pin_green = 2;
-const byte int_pin_red = 3;
+const byte int_pin_green = D6;
+const byte int_pin_red = D5;
 
 unsigned long timer_green;
 unsigned long timer_red;
 
-SSD1306 display(0x3c, D3, D5);
+//WiFiServer server(80);
+//WiFiClient client;
 
-char * line[4];
+SSD1306 display(0x3c, SDA, SCL);
+
+char * line[MAX_LINES];
 int vote_green, vote_red;
-
-/* Write text to display */
-void setText(String text, int x = 0, int y = 0)
-{
-    display.clear();
-    display.drawString(x, y, text);
-    display.display();
-}
+bool update;
 
 void setup()
 {
-    Serial.begin(9600);
-    Serial.setTimeout(1000);
-  
     display.init();
     display.flipScreenVertically();
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.setFont(ArialMT_Plain_10);
 
+    Serial.begin(9600);
+    //Serial.setTimeout(1000);
+    delay(10);
+
     /* Interrupt pins */
     pinMode(int_pin_green, INPUT_PULLUP);
     pinMode(int_pin_red, INPUT_PULLUP);
 
-    /* Setup interrupts */
-    attachInterrupt(digitalPinToInterrupt(int_pin_green), int_func_green, LOW);
-    attachInterrupt(digitalPinToInterrupt(int_pin_red), int_func_red, LOW);
+    attachInterrupt(int_pin_green, int_func_green, FALLING);
+    attachInterrupt(int_pin_red, int_func_red, FALLING);
 
     timer_red = millis();
     timer_green = millis();
-/*
-    for (int i = 0; i < 4; i++)
+
+    for (int i = 0; i < MAX_LINES; i++)
     {
-        line[i] = malloc(10);
-    }*/
+        line[i] = (char *)malloc(LINE_LENGHT);
+    }
 
     strcpy(line[0], "Set text");
     strcpy(line[1], "with APP!");
     strcpy(line[2], "\0");
     strcpy(line[3], "\0");
+
+    updateScreen();
+
+    update = false;
 }
 
 void loop()
@@ -62,37 +103,35 @@ void loop()
     char * data;
     data = readSerial();
 
-    setText("Joeläger", 0, 0);
-
     /* Switch on first char in sent command */
-    /*switch (data[0])
+    switch (data[0])
     {
         case '1':  // Line 1
             strcpy(line[0], data + 1);
-            setText();
+            update = true;
             Serial.println("Line 1 set");
             break;
 
         case '2': // Line 2
             strcpy(line[1], data + 1);
-            setText();
+            update = true;
             Serial.println("Line 2 set");
             break;
 
         case '3': // Line 3
             strcpy(line[2], data + 1);
-            setText();
+            update = true;
             Serial.println("Line 3 set");
             break;
 
         case '4': // Line 4
             strcpy(line[3], data + 1);
-            setText();
+            update = true;
             Serial.println("Line 4 set");
             break;
 
         case 'C': // Clear/Reset
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < MAX_LINES; i++)
             {
                 strcpy(line[i], "\0");
             }
@@ -100,23 +139,27 @@ void loop()
             vote_red = 0;
             vote_green = 0;
 
-            setText();
+            update = true;
 
             break;
 
         case 'S': // Status
             /* TODO: Add current display data */
-            /*Serial.println("Connected to voteMachine!");
+            Serial.println("Connected to voteMachine!");
             Serial.println("GREEN: " + String(vote_green));
             Serial.println("RED: " + String(vote_red));
             break;
 
         default:
             break;
-    }*/
+    }
 
+    if(update)
+    {
+        updateScreen();
+        update = false;
+    }
 }
-
 
 /* Read serial command (from bluetooth) */
 char * readSerial()
@@ -131,11 +174,7 @@ char * readSerial()
 
     if(command_size > 0)
     {
-        /* Show command size on display */
-        sprintf(line[3], "CMD SIZE: %i", command_size);
-
-        command[command_size] = 0;
-
+        command[command_size] = '\0';
         return command;
     }
 
@@ -143,7 +182,20 @@ char * readSerial()
 
 }
 
+/* Write text to display */
+void updateScreen()
+{
+    sprintf(line[MAX_LINES-1], "Green: %i | Red: %i", vote_green, vote_red);
 
+    display.clear();
+
+    for(int i = 0; i < MAX_LINES; i++)
+    {
+        display.drawString(0, LINE_HEIGHT * i,  String(line[i]));
+    }
+
+    display.display();
+}
 
 /* Green button interrupt function */
 void int_func_green()
@@ -152,9 +204,7 @@ void int_func_green()
     {
         vote_green++;
         Serial.println("Green votes: " + String(vote_green));
-
-        sprintf(line[3], "GR: %i RE: %i", vote_green, vote_red);
-
+        update = true;
         timer_green = millis();
     }
 }
@@ -166,9 +216,7 @@ void int_func_red()
     {
         vote_red++;
         Serial.println("Red votes: " + String(vote_red));
-
-        sprintf(line[3], "GR: %i RE: %i", vote_green, vote_red);
-
+        update = true;
         timer_red = millis();
     }
 }
