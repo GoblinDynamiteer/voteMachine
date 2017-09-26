c3a0e22c5a2e7cfaeeea78a196175a07186c6fef
Johan Kämpe <kampe.johan@gmail.com>
2017-09-26 17:23:55 +0200

    WPF verison of APP

---
+++ voteMachine_App_WPF/voteApp/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace voteApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] ports;
+        SerialPort serialPort;
+        string serialData;
+        public delegate void NextPrimeDelegate();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort();
+            serialPort.BaudRate = 115200;
+            this.serialPort.DataReceived += new
+                SerialDataReceivedEventHandler(serialPort_DataReceived);
+
+            UpdateCOMportList();
+
+        }
+
+        public void UpdateQuestion(
+            string question, string optionRed, string optionGreen)
+        {
+            textQuestion.Text = question;
+            lblGreenOption.Content = optionGreen;
+            lblRedOption.Content = optionRed;
+        }
+
+        /* Update drop-down list with available COM-ports */
+        void UpdateCOMportList(bool getDeviceNames = false)
+        {
+            ports = SerialPort.GetPortNames();
+
+            Array.Sort(ports);
+
+            listboxCOM.Items.Clear();
+
+            /* Populate combobox with ports 
+             * (port names and device names) */
+            foreach (string port in ports)
+            {
+                listboxCOM.Items.Add(port);
+            }
+
+        }
+
+        /* Open COM-port selected in list */
+        private void btnOpenCOM_Click(
+            object sender, RoutedEventArgs e)
+        {
+            string comPort = listboxCOM.Text;
+
+            if (serialPort.PortName != comPort)
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                }
+
+                OpenCOM(comPort);
+
+            }
+        }
+
+        /* Open COM-Port */
+        bool OpenCOM(string portName)
+        {
+            bool success = true;
+
+            if (!serialPort.IsOpen)
+            {
+
+                try
+                {
+                    serialPort.PortName = portName;
+                    serialPort.Open();
+
+                    textboxStatus.AppendText(
+                        "Port " + serialPort.PortName +
+                        " öppnad!\r\n");
+                }
+
+                catch (Exception)
+                {
+                    textboxStatus.AppendText("Kunde inte öppna " 
+                        + serialPort.PortName + "!");
+                    success = false;
+                }
+
+            }
+
+            return success;
+        }
+
+        /* Event method for serial data available */
+        private void serialPort_DataReceived(
+            object sender, SerialDataReceivedEventArgs e)
+        {
+            serialData = serialPort.ReadLine();
+            this.Dispatcher.Invoke(DispatcherPriority.Normal,
+                new NextPrimeDelegate(DisplayText));
+        }
+
+        /* Display serial data and misc */
+        private void DisplayText()
+        {
+
+            /* Display data in textbox */
+            textboxStatus.AppendText(serialData);
+
+            if (checkboxAutoScroll.IsChecked == true)
+            {
+                textboxStatus.ScrollToEnd();
+            }
+
+            if (serialData.Contains("votes"))
+            {
+                UpdateVoteLabels();
+            }
+        }
+
+        private void Button_Click(
+            object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+                "Är du säker på att du vill återställa?", 
+                "Återställning", 
+                MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes && serialPort.IsOpen)
+            {
+                serialPort.WriteLine("C");
+            }
+        }
+
+        private void btnNewQuestion_Click(
+            object sender, RoutedEventArgs e)
+        {
+            Window newQuestion = new setQuestion(this, serialPort);
+            newQuestion.Show();
+        }
+
+        /* Filter votes, update labels */
+        private void UpdateVoteLabels()
+        {
+            string number = Regex.Match(
+                serialData, @"\d+").Value;
+
+            if (serialData.Contains("Green"))
+            {
+                lblGreenVotes.Content = number;
+            }
+
+            if (serialData.Contains("Red"))
+            {
+                lblRedVotes.Content = number;
+            }
+
+        }
+    }
+}
---
+++ voteMachine_App_WPF/voteApp/setQuestion.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System.IO.Ports;
+using System.Windows;
+
+namespace voteApp
+{
+    /// <summary>
+    /// Interaction logic for setQuestion.xaml
+    /// </summary>
+    public partial class setQuestion : Window
+    {
+        SerialPort serialPort;
+        MainWindow mainWindow;
+        string redTextDefault = "RÖTT VAL";
+        string greenTextDefault = "GRÖNT VAL";
+        string inputTextDefault = "Fråga";
+
+        public setQuestion(MainWindow mainWindow, SerialPort serialPort)
+        {
+            InitializeComponent();
+            this.serialPort = serialPort;
+            this.mainWindow = mainWindow;
+
+            textBoxGreenOpt.Text = greenTextDefault;
+            textBoxRedOpt.Text = redTextDefault;
+            textBoxInput.Text = inputTextDefault;
+        }
+
+        private void btnSendQuestion_Click(
+            object sender, RoutedEventArgs e)
+        {
+            string command = "";
+
+            char[] replaceChars = { 'å', 'ä', 'ö', 'Å', 'Ä', 'Ö' };
+            char[] replaceWith = { 'a', 'a', 'o', 'A', 'A', 'O' };
+
+            command += "R" + textBoxRedOpt.Text + "\r\n";
+            command += "G" + textBoxGreenOpt.Text + "\r\n";
+
+            for (int i = 0; i < textBoxInput.LineCount || i < 4; i++)
+            {
+                command += (i+1) + textBoxInput.GetLineText(i) + "\r\n";
+            }
+
+            /* Replace ÅÄÖåäö with AaOo */
+            for (int i = 0; i < replaceChars.Length; i++)
+            {
+                command = command.Replace(replaceChars[i], replaceWith[i]);
+            }
+
+            /* Send command if serialport is open */
+            if (command != "" && serialPort.IsOpen)
+            {
+                serialPort.WriteLine(command);
+            }
+
+            /* Update labels in main window */
+            mainWindow.UpdateQuestion(
+                textBoxInput.Text, textBoxRedOpt.Text, textBoxGreenOpt.Text);
+
+            this.Close();
+        }
+
+        private void btnCancel_Click(
+            object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBoxRedOpt_GotFocus(
+            object sender, RoutedEventArgs e)
+        {
+            if (textBoxRedOpt.Text == redTextDefault)
+            {
+                textBoxRedOpt.Clear();
+            }
+        }
+
+        private void textBoxRedOpt_LostFocus(
+            object sender, RoutedEventArgs e)
+        {
+            if (textBoxRedOpt.Text == "")
+            {
+                textBoxRedOpt.Text = redTextDefault;
+            }
+        }
+
+        private void textBoxGreenOpt_GotFocus(
+            object sender, RoutedEventArgs e)
+        {
+            if (textBoxGreenOpt.Text == greenTextDefault)
+            {
+                textBoxGreenOpt.Clear();
+            }
+        }
+
+        private void textBoxGreenOpt_LostFocus(
+            object sender, RoutedEventArgs e)
+        {
+            if (textBoxGreenOpt.Text == "")
+            {
+                textBoxGreenOpt.Text = greenTextDefault;
+            }
+        }
+
+        private void textBoxInput_GotFocus(
+            object sender, RoutedEventArgs e)
+        {
+            if (textBoxInput.Text == inputTextDefault)
+            {
+                textBoxInput.Text = "";
+            }
+        }
+    }
+}
